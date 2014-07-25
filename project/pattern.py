fc917e6b0a5192a134af40e9330bcaf0bce905c9
Connie Nguyen <knguy068@ucr.edu>
2014-07-25 15:26:31 -0700

    Processes image using a mini version of input file. Need to add error checking. maxColors not working.

--- project/pattern.py
+++ project/pattern.py
@@ -1,18 +1,17 @@
+import math
 from PIL import Image
 
 COLOR_BLACK = (0,0,0)
 COLOR_GREY = (128, 128, 128)
 
 # Colors an blockSize x blockSize pixel block + outlining grid
-# TO DO: Change to take in a color
 def colorBlock(blockSize, blockColor, upperLeft, outImage):
-    midBlock = int(blockSize / 2)
     for i in range(0, blockSize):
 	for j in range(0, blockSize):
 	    putx = upperLeft[0] + i
 	    puty = upperLeft[1] + j
 	    if i == blockSize-1 or j == blockSize-1:
-		    outImage.putpixel((putx, puty), COLOR_GREY)
+		outImage.putpixel((putx, puty), COLOR_GREY)
 	    else:
 		outImage.putpixel((putx, puty), blockColor)
     # Adjust grid color if tenth block
@@ -31,9 +30,10 @@ def colorBlock(blockSize, blockColor, upperLeft, outImage):
 # Takes in the input and the output Images and colors blocks
 # in the output Image based in the input Image. The two images
 # may no be the same size
-def generatePattern(stitchSize, blockSize, inImage, outImage):
-    midStitch = int(stitchSize / 2)
+def generatePattern(stitches, blockSize, inImage, outImage):
     inWidth, inHeight = inImage.size
+    stitchSize = inWidth / stitches
+    midStitch = stitchSize / 2
     inWidth = int(inWidth / stitchSize)
     inHeight = int(inHeight / stitchSize)
     outWidth, outHeight = outImage.size
@@ -44,21 +44,24 @@ def generatePattern(stitchSize, blockSize, inImage, outImage):
 	    blockColor = inImage.getpixel((getx, gety))
 	    outxy = (i * blockSize, j * blockSize)
 	    colorBlock(blockSize, blockColor, outxy, outImage)
+	
+def processImage(filename='pikachu.png', stitches=60, maxColors=256):
+    uploadedImage = Image.open('uploads/' + filename)
+    width, height = uploadedImage.size
+    width = float(width)
+    height = float(height)
+    # Resize image according to stitches across
+    ratio = width / height
+    miniw = stitches * 8
+    minih = int(math.ceil(miniw / ratio))
+    stitchSize = int(miniw / stitches)
+    stitchesh = int(minih / stitchSize)
+    miniImage = uploadedImage.resize((int(miniw), int(minih)), Image.ANTIALIAS)
+    miniImage.show()
+    miniImage = miniImage.convert('RGB', palette=Image.ADAPTIVE, colors=maxColors)
+    blockSize = 25
+    saveImage = Image.new('RGB', (blockSize*stitches, blockSize*stitchesh), COLOR_BLACK)
+    generatePattern(stitches, blockSize, miniImage, saveImage)
+    saveImage.show()
 
-filename = 'plant.png'
-stitches = 60
-maxColors = 10
-uploadedImage = Image.open('uploads/' + filename)
-width, height = uploadedImage.size
-blockSize = int(width / stitches)
-width = int(width / blockSize) * blockSize
-height = int(height / blockSize) * blockSize
-processImage = uploadedImage.resize((width, height), Image.ANTIALIAS)
-processImage = processImage.convert('P', palette=Image.ADAPTIVE, colors=maxColors)
-#adjustedImage = uploadedImage.convert('P', palette=Image.ADAPTIVE, colors=maxColors)
-processImage.putalpha(0)
-colors = processImage.getcolors(maxColors)
-#processImage = adjustedImage.resize((width, height), Image.ANTIALIAS)
-patternImage = processImage.resize((int(width/blockSize) * 25, int(height/blockSize) * 25), Image.ANTIALIAS)
-generatePattern(blockSize, 25, processImage, patternImage)
-patternImage.show()
+processImage('pikachu.png', 50, 3)
