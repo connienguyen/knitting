bcde93022fd8fca23b6e84178cb44bc859cdbaea
Connie Nguyen <knguy068@ucr.edu>
2014-07-24 19:36:34 -0700

    Grid coloring is working correctly

--- project/pattern.py
+++ project/pattern.py
@@ -6,23 +6,38 @@ stitches = 50
 baseImage = Image.open('uploads/' + filename)
 COLOR_BLACK = (0,0,0)
 COLOR_GREY = (128, 128, 128)
+COLOR_RED = (255, 0, 0)
 
-# Make a function which colors an nxn pixel block of the output image
+# Colors an blockSize x blockSize pixel block + outlining grid
 def colorBlock(blockSize, upperLeft, inImage, outImage):
     midBlock = int(blockSize / 2)
     blockColor = inImage.getpixel((upperLeft[0] + midBlock, upperLeft[1] + midBlock))
     for i in range(0, blockSize):
 	for j in range(0, blockSize):
-	    if i == blockSize -1 or j == blockSize -1:
-		outImage.putpixel((upperLeft[0]+i, upperLeft[1]+j), COLOR_GREY)
+	    putx = upperLeft[0] + i
+	    puty = upperLeft[1] + j
+	    if i == blockSize-1 or j == blockSize-1:
+		    outImage.putpixel((putx, puty), COLOR_GREY)
 	    else:
-		outImage.putpixel((upperLeft[0] + i, upperLeft[1] + j), blockColor)
+		outImage.putpixel((putx, puty), blockColor)
+    # Adjust grid color if tenth block
+    tenth = blockSize * 10
+    if (upperLeft[0] + blockSize) % tenth == 0:
+	putx = upperLeft[0] + blockSize - 1
+	for j in range(0, blockSize):
+	    puty = upperLeft[1] + j
+	    outImage.putpixel((putx, puty), COLOR_BLACK)
+    if (upperLeft[1] + blockSize) % tenth == 0:
+	puty = upperLeft[1] + blockSize -1
+	for i in range(0, blockSize):
+	    putx = upperLeft[0] + i
+	    outImage.putpixel((putx, puty), COLOR_BLACK)
 
 result = baseImage.convert('P', palette=Image.ADAPTIVE, colors=3)
 result.putalpha(0)
 colors = result.getcolors(3)
 width, height = result.size
-blockSize = 5
+blockSize = 25
 width = int(width / blockSize) * blockSize
 height = int(height / blockSize) * blockSize
 for i in range(0, width):
